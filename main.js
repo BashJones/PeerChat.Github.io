47826f87a0ffc854d44fe18dc19897e015a7c382
BashJones <45294075+BashJones@users.noreply.github.com>
2023-10-15 00:57:35 +0100

    User join... possible errors..

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//Yeeeeeah best replace with token authentication
+let APP_ID = "0a6cf4470e0442939e5917ec50dbd8a8";
+
+//pass this in next weekend!
+let token = null;
+//generate a random ID number for now mehhhh
+let uid = String(Math.floor(Math.random() * 1000))
+
+//create client object we can log in with
+let client;
+//The channel that the users actually join
+let channel;
+
 let localStream;
 let remoteStream;
 let peerConnection;
@@ -16,38 +29,61 @@ const servers = {
  * pass in the parameters of what you want access to
  */
 let init = async () => {
+
+    //Create client object
+    client = await AgoraRTM.createInstance(APP_ID)
+    //The token value is null till you decide to implement it
+    await client.login({uid, token})
+
+    //change main to room ID once you figure out how
+    //Create channel and jon functionality
+    channel = client.createChannel('main')
+    await channel.join()
+
+    //Event listener for when the join method is called
+    channel.on('MemberJoined', handleUserJoined)
+
     localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
     document.getElementById('user-1').srcObject = localStream
 
     createOffer()
 }
+
+//Function to handle users who join
+let handleUserJoined = async (MemberID) => {
+    console.log('A new hero joined the channel', MemberID)
+}
+
+
+
+
 /**
  * peerConnection stores all info/methods to connect between peers
  * remoteStream sets up media stream for other user
  * offer - each peerConnection has an offer/answer (SDP)
  */
 let createOffer = async () => {
-//pass in the servers
+    //pass in the servers
     peerConnection = new RTCPeerConnection(servers)
 
     remoteStream = new MediaStream()
     document.getElementById('user-2').srcObject = remoteStream
 
-//loop through all the a/v tracks and add them to PeerConnection so remotePeer can access them
+    //loop through all the a/v tracks and add them to PeerConnection so remotePeer can access them
     localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream)
     });
 
-//listen for anytime remote peer adds track
-//looping through every single track from remote peer
+    //listen for anytime remote peer adds track
+    //looping through every single track from remote peer
     peerConnection.ontrack = (event) => {
         event.streams[0].getTracks().forEach((track) => {
-//Adding the track to the remote/peer stream 
+    //Adding the track to the remote/peer stream 
             remoteStream.addTrack(track)
         })
     }
 
-//Generate ICE candidates
+    //Generate ICE candidates
     peerConnection.onicecandidate = async (event) => {
         if(event.candidate){
             console.log('New ICE Candidate', event.candidate)
@@ -55,7 +91,7 @@ let createOffer = async () => {
     }
 
     let offer = await peerConnection.createOffer()
-//Trigger STUN server and create candidates
+    //Trigger STUN server and create candidates
     await peerConnection.setLocalDescription(offer)
 
 
