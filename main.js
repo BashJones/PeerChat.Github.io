60e333950e27c345138f4aa7aaeb30ced603c6eb
BashJones <45294075+BashJones@users.noreply.github.com>
2023-10-14 13:28:21 +0100

    Setup stream

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("JavaScript is ready to run");
+let localStream;
+let remoteStream;
+
+/**
+ * The getUserMedia func carries out the request for cam/mic perms
+ * pass in the parameters of what you want access to
+ */
+let init = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    document.getElementById('user-1').srcObject = localStream
+}
+
+init()
